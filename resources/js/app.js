54ce197c6ec86eb8326cfda1ab3e9c3e68c90ef5
Scorpio Software <scorpiosoftware666@gmail.com>
2025-05-16 20:53:32 +0300

    Implement code changes to enhance functionality and improve performance

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,13 +8,13 @@ import './slider.js';
 // import './textarea.js';
 import   './echo.js'
 import './toast.js';
-import 'flowbite';
+// import 'flowbite';
 import './wow.js';
 import './imageCompressor.js';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] =
-    document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+// window.axios.defaults.headers.common['X-CSRF-TOKEN'] =
+//     document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 import Alpine from 'alpinejs';
 window.$ = window.jQuery = $;
 window.Alpine = Alpine;
